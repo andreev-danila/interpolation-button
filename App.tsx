d42f11a13673627f5281284a446d3faaf8bd5847
Dan Andreev <andrdan.vue@gmail.com>
2024-05-23 14:45:15 +0400

    init
    Signed-off-by: Dan Andreev <andrdan.vue@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
-import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx" />
       <StatusBar style="auto" />
     </>
   );
